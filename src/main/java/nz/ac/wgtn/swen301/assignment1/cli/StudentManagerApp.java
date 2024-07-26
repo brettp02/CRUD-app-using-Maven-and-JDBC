4b34a89a08b787b18a43fdd607f589ad31048b89
brettp02 <brettpenwarden@gmail.com>
2024-07-26 12:21:30 +1200

    init

---
+++ src/main/java/nz/ac/wgtn/swen301/assignment1/cli/StudentManagerApp.java
@@ -0,0 +1,12 @@
+package nz.ac.wgtn.swen301.assignment1.cli;
+
+public class StudentManagerApp {
+
+    // THE FOLLOWING METHOD MUST BE IMPLEMENTED
+    /**
+     * Executable: the user will provide argument(s) and print details to the console as described in the assignment brief,
+     * E.g. a user could invoke this by running "java -cp <someclasspath> <arguments></arguments>"
+     * @param arg
+     */
+    public static void main (String[] arg) {}
+}
